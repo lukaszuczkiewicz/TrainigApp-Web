979a8279096abc818a0c71694b17b517f13cc7cb
lukasz <lukaszuczkiewicz@gmail.com>
2020-02-01 03:11:05 +0100

    add  runner list pagination and sorting

--- src/app/modules/runners/services/runners.service.ts
+++ src/app/modules/runners/services/runners.service.ts
@@ -12,15 +12,28 @@ export class RunnersService {
   private readonly URL = environment.URL;
   private readonly runnersEndpoint = environment.runnersEndpoint;
   private readonly addRunnerEndpoint = environment.addRunner;
+  private readonly editRunnerEndpoint = environment.editRunner;
+  private readonly deleteRunnerEndpoint = environment.deleteRunner;
 
   constructor(private http: HttpClient) {}
+
+  public createRunner(runner: RunnerToCreate): Observable<any>  {
+    const url: string = this.URL + this.addRunnerEndpoint;
+    return this.http.post(url, runner);
+  }
+
+  public deleteRunner(runnerId: string): Observable<any>  {
+    const url: string = this.URL + this.deleteRunnerEndpoint;
+    return this.http.post(url, runnerId);
+  }
+
+  public editRunner(runner: IRunner) {
+    const url: string = this.URL + this.editRunnerEndpoint;
+    return this.http.put(url, runner);
+  }
 
   public getRunners(): Observable<IRunner[]> {
     const url: string = this.URL + this.runnersEndpoint;
     return this.http.get<IRunner[]>(url);
-  }
-  public createRunner(runner: RunnerToCreate): Observable<any>  {
-    const url: string = this.URL + this.addRunnerEndpoint;
-    return this.http.post(url, runner);
   }
 }
